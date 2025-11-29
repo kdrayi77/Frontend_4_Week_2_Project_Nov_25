0f4279e52ad143ef33ae26759c929388837f126f
Gracerayi <kdrayi77@gmail.com>
2025-11-29 14:55:43 +0530

    Initial commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.js
+
+import React from "react";
+import PincodeLookup from "./components/PincodeLookup";
+
+function App() {
+  return (
+    <div>
+      <PincodeLookup />
+    </div>
+  );
+}
+
+export default App;
